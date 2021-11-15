1fe847764fc0912e3810aad12c68b1428e6bea29
sassi <attila.sassi@gmail.com>
2021-11-15 23:53:27 +0100

    kész feladat

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,8 @@
+const converterPromise = arrayOfStrings => new Promise((resolve, reject) => {
+  if (!Array.isArray(arrayOfStrings) || !arrayOfStrings.every(e => typeof e == 'string'))
+    reject('Error: Not all elements are string type!');
+  else resolve(arrayOfStrings.map(e => e.toUpperCase()));
+});
 
 
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
